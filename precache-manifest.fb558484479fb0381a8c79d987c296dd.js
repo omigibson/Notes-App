189ac71319df63347bc01e05350164f5190b8c1f
Omi Gibba <oumie.gibba@gmail.com>
2020-06-07 19:10:25 -0700

    Updates

---
+++ precache-manifest.fb558484479fb0381a8c79d987c296dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eb81ebee5e6197845777bc84ab630f1",
+    "url": "/Notes-App/index.html"
+  },
+  {
+    "revision": "f83365226bd317273e6d",
+    "url": "/Notes-App/static/css/main.1ddd0b88.chunk.css"
+  },
+  {
+    "revision": "17470b82f6b5698fd5db",
+    "url": "/Notes-App/static/js/2.f6a860b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Notes-App/static/js/2.f6a860b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f83365226bd317273e6d",
+    "url": "/Notes-App/static/js/main.2bd5451e.chunk.js"
+  },
+  {
+    "revision": "e71304c358cc440454b7",
+    "url": "/Notes-App/static/js/runtime-main.89014c62.js"
+  }
+]);
--- src/components/button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Button (props) {
-   return (
-      <button type="button" className={props.cls} onClick={() => props.onClick(props.noteId)}>{props.buttonText}</button>
-   )
-}
-
-export default Button
